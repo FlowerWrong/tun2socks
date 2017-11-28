a7839dea16ad3ec0403ed275e62ed2d1462e834d
flowerwrong <sysuyangkang@gmail.com>
2017-11-28 19:04:39 +0800

    todo udp server

--- main.go
+++ main.go
@@ -1,10 +1,3 @@
-// Copyright 2016 The Netstack Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// This sample creates a stack with TCP and IPv4 protocols on top of a TUN
-// device, and listens on a port. Data received by the server in the accepted
-// connections is echoed back to the clients.
 package main
 
 import (
@@ -28,6 +21,8 @@ import (
 	"os/exec"
 	"runtime"
 	"github.com/FlowerWrong/tun2socks/socket"
+	"sync"
+	"github.com/FlowerWrong/netstack/tcpip/transport/udp"
 )
 
 func execCommand(name, sargs string) error {
@@ -51,13 +46,12 @@ func addLinuxRoute(tun string, subnet *net.IPNet) error {
 }
 
 func main() {
-	if len(os.Args) != 4 {
-		log.Fatal("Usage: ", os.Args[0], " <tun-device> <local-address> <local-port>")
+	if len(os.Args) != 3 {
+		log.Fatal("Usage: ", os.Args[0], " <local-address> <local-port>")
 	}
 
-	// tunName := os.Args[1]
-	addrName := os.Args[2]
-	portName := os.Args[3]
+	addrName := os.Args[1]
+	portName := os.Args[2]
 
 	rand.Seed(time.Now().UnixNano())
 
@@ -88,7 +82,7 @@ func main() {
 
 	// Create the stack with ip and tcp protocols, then add a tun-based
 	// NIC and address.
-	s := stack.New([]string{ipv4.ProtocolName, ipv6.ProtocolName}, []string{tcp.ProtocolName})
+	s := stack.New([]string{ipv4.ProtocolName, ipv6.ProtocolName}, []string{tcp.ProtocolName, udp.ProtocolName})
 
 	var mtu uint32 = 1500
 
@@ -113,11 +107,6 @@ func main() {
 			log.Println(err)
 			return
 		}
-
-		//_, ipnet1, _ := net.ParseCIDR("119.23.10.5")
-		//addLinuxRoute("10.0.0.1", ipnet1)
-		//_, ipnet2, _ := net.ParseCIDR("47.90.32.252")
-		//addLinuxRoute("192.168.1.1", ipnet2)
 	} else {
 		log.Println("not support os")
 		return
@@ -142,19 +131,60 @@ func main() {
 		},
 	})
 
-	// Create TCP endpoint, bind it, then start listening.
+	var waitGroup sync.WaitGroup
+	//waitGroup.Add(1)
+	//go NewTCPEndpointAndListenIt(s, proto, localPort, waitGroup)
+	waitGroup.Add(1)
+	go NewUDPEndpointAndListenIt(s, proto, localPort, waitGroup)
+	waitGroup.Wait()
+}
+
+// Create UDP endpoint, bind it, then start listening.
+func NewUDPEndpointAndListenIt(s *stack.Stack, proto tcpip.NetworkProtocolNumber, localPort int, waitGroup sync.WaitGroup) {
 	var wq waiter.Queue
-	ep, e := s.NewEndpoint(tcp.ProtocolNumber, proto, &wq)
-	if err != nil {
+	ep, e := s.NewEndpoint(udp.ProtocolNumber, proto, &wq)
+	if e != nil {
 		log.Fatal(e)
 	}
-
 	defer ep.Close()
-
-	if err := ep.Bind(tcpip.FullAddress{0, "", uint16(localPort)}, nil); err != nil {
+	defer waitGroup.Done()
+	if err := ep.Bind(tcpip.FullAddress{0, "", uint16(localPort + 1)}, nil); err != nil {
 		log.Fatal("Bind failed: ", err)
 	}
 
+	// Wait for connections to appear.
+	waitEntry, notifyCh := waiter.NewChannelEntry(nil)
+	wq.EventRegister(&waitEntry, waiter.EventIn)
+	defer wq.EventUnregister(&waitEntry)
+
+	for {
+		var addr tcpip.FullAddress
+		v, err := ep.Read(&addr)
+		if err != nil {
+			if err == tcpip.ErrWouldBlock {
+				<-notifyCh
+				continue
+			}
+
+			log.Fatal("Read failed:", err)
+		}
+		log.Println(v)
+	}
+}
+
+// Create TCP endpoint, bind it, then start listening.
+func NewTCPEndpointAndListenIt(s *stack.Stack, proto tcpip.NetworkProtocolNumber, localPort int, waitGroup sync.WaitGroup) {
+	var wq waiter.Queue
+	ep, err := s.NewEndpoint(tcp.ProtocolNumber, proto, &wq)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ep.Close()
+	defer waitGroup.Done()
+	if err := ep.Bind(tcpip.FullAddress{0, "", uint16(localPort)}, nil); err != nil {
+		log.Fatal("Bind failed: ", err)
+	}
 	if err := ep.Listen(1024); err != nil {
 		log.Fatal("Listen failed: ", err)
 	}
